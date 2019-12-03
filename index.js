75798a953598a34a8378ac4988fb9dbeecbdbcea
Osgood Gunawan <osgoodgunawan@hotmail.com>
2019-12-03 03:35:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // add solution here
 function theBeatlesPlay(music,instrument){
   var result=[];
-  for(let i=0;)
+  for(let i=0;i<music.length;i++){
+    music[i]+instrument[i]
+  };
 }
+
+
+function johnLennonFacts()
