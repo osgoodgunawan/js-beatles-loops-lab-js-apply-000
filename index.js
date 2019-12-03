6f5ec9191cc21631f6060302f4120f8536e979b4
Osgood Gunawan <osgoodgunawan@hotmail.com>
2019-12-03 03:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ function theBeatlesPlay(musicians,instruments){
 
 function johnLennonFacts(){
   const facts = [
-  "foo",
-  "bar"
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
 ];
 
 let final=[];
